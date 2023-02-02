539ddf1c1e45562e2ce73b917aefebe40f942b51
MisaelDeAraujoSilva <misael.de.araujo.silva@gmail.com>
2023-02-02 07:59:46 -0300

    Atualizado

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,17 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosListagemPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import med.voll.api.paciente.DadosCadastroPaciente;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -29,4 +25,8 @@ public class PacienteController {
 		return repository.findAll(paginacao).map(DadosListagemPaciente::new); //:: method reference referencia new que é a dto DadosLIstagemPaciente, que pega todos os dados da entidade.
 	}
 
+	public void atualizar (DadosAtualizacaoPaciente dados){
+		var paciente = repository.getReferenceById(dados.id());
+		paciente.atualizarInformacoes(dados);
+	}
 }
